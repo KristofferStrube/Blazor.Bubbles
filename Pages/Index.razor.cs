5afe4b92a916f4a9ea7ea48cc1ad0ef02e9b43a8
Kristoffer Strube <kristoffer.strube@gmail.com>
2022-01-12 23:48:53 +0100

    init

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,185 @@
+using Blazor.Bubbles.Services;
+using Microsoft.AspNetCore.Components;
+using System.Text.Json;
+
+namespace Blazor.Bubbles.Pages
+{
+    public partial class Index
+    {
+        [Parameter, SupplyParameterFromQuery]
+        public double? Amount { get; set; }
+
+        [Parameter, SupplyParameterFromQuery]
+        public double? MinRadius { get; set; }
+
+        [Parameter, SupplyParameterFromQuery]
+        public double? MaxRadius { get; set; }
+
+        [Parameter, SupplyParameterFromQuery]
+        public double? MaxSpeed { get; set; }
+
+        [Parameter, SupplyParameterFromQuery]
+        public string? Colors { get; set; }
+
+        [Inject]
+        protected BoundingBoxSubscriberService BoundingBoxSubscriberService { get; set; }
+
+        protected ElementReference Div { get; set; }
+
+        protected BoundingBox BoundingBox { get; set; } = new();
+
+        protected List<Bubble> Bubbles = new();
+
+        protected Random Random = new Random();
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                BoundingBoxSubscriber? subscriber = await BoundingBoxSubscriberService.CreateSubscriberForChangeAsync();
+                await subscriber.Subscribe(Div, (b) =>
+                {
+                    BoundingBox = b;
+                    StateHasChanged();
+                });
+
+                if (!Amount.HasValue)
+                {
+                    Amount = 30;
+                }
+
+                if (!MinRadius.HasValue)
+                {
+                    MinRadius = Math.Max(BoundingBox.Width, BoundingBox.Height) / 20 / 10;
+                }
+
+                if (!MaxRadius.HasValue)
+                {
+                    MaxRadius = Math.Max(BoundingBox.Width, BoundingBox.Height) / 20;
+                }
+
+                if (MinRadius > MaxRadius)
+                {
+                    throw new ArgumentException("MinRadius needs to be lower than or equal to MaxRadius. MinRadius is 0 by default and MaxRadius is one 20th of the widest of the height or width of the screen.");
+                }
+
+                if (!MaxSpeed.HasValue)
+                {
+                    MaxSpeed = 1;
+                }
+
+                List<string> colors = new() { "#007A30", "#00C44E", "#00DB57", "#00A843", "#00913A", "#00B749" };
+                if (Colors is not null)
+                {
+                    colors = JsonSerializer.Deserialize<List<string>>(Colors) ?? colors;
+                }
+
+                for (int i = 0; i < Amount; i++)
+                {
+                    Bubbles.Add(new Bubble()
+                    {
+                        Color = colors[Random.Next(colors.Count)],
+                        Radius = MinRadius.Value + Random.NextDouble() * (MaxRadius.Value - MinRadius.Value),
+                        X = Random.NextDouble() * BoundingBox.Width,
+                        Y = Random.NextDouble() * BoundingBox.Height,
+                        VX = Random.NextDouble() * (Random.Next(0, 2) == 0 ? 1 : -1) * MaxSpeed.Value,
+                        VY = Random.NextDouble() * (Random.Next(0, 2) == 0 ? 1 : -1) * MaxSpeed.Value
+                    });
+                }
+
+                await Task.Run(async () =>
+                {
+                    while (true)
+                    {
+                        await Task.Delay(1);
+                        foreach (Bubble b in Bubbles)
+                        {
+                            b.X += b.VX;
+                            b.Y += b.VY;
+                            CollideWithOuterBox(b);
+                        }
+                        foreach (Bubble b1 in Bubbles)
+                        {
+                            foreach (Bubble b2 in Bubbles)
+                            {
+                                if (b1 == b2)
+                                {
+                                    continue;
+                                }
+
+                                CollideBubbles(b1, b2);
+                            }
+                        }
+
+                        StateHasChanged();
+                    }
+                });
+            }
+        }
+
+        protected void RandomizeBubble(Bubble b)
+        {
+            b.Radius = MinRadius.Value + Random.NextDouble() * (MaxRadius.Value - MinRadius.Value);
+            b.VX = Random.NextDouble() * (Random.Next(0, 2) == 0 ? 1 : -1) * MaxSpeed.Value;
+            b.VY = Random.NextDouble() * (Random.Next(0, 2) == 0 ? 1 : -1) * MaxSpeed.Value;
+        }
+
+        private void CollideWithOuterBox(Bubble b)
+        {
+            if (b.X - b.Radius < 0)
+            {
+                b.X -= b.X - b.Radius;
+                b.VX = Math.Abs(b.VX);
+            }
+            if (b.X + b.Radius > BoundingBox.Width)
+            {
+                b.X -= b.X + b.Radius - BoundingBox.Width;
+                b.VX = -Math.Abs(b.VX);
+            }
+            if (b.Y - b.Radius < 0)
+            {
+                b.Y -= b.Y - b.Radius;
+                b.VY = Math.Abs(b.VY);
+            }
+            if (b.Y + b.Radius > BoundingBox.Height)
+            {
+                b.Y -= b.Y + b.Radius - BoundingBox.Height;
+                b.VY = -Math.Abs(b.VY);
+            }
+        }
+
+        private void CollideBubbles(Bubble b1, Bubble b2)
+        {
+            double dist = Math.Sqrt(Math.Pow(b1.X - b2.X, 2) + Math.Pow(b1.Y - b2.Y, 2));
+            if (dist > b1.Radius + b2.Radius)
+            {
+                return;
+            }
+
+            b1.X += (b1.X - b2.X) / dist * (b1.Radius + b2.Radius - dist);
+            b1.Y += (b1.Y - b2.Y) / dist * (b1.Radius + b2.Radius - dist);
+            b2.X += (b2.X - b1.X) / dist * (b1.Radius + b2.Radius - dist);
+            b2.Y += (b2.Y - b1.Y) / dist * (b1.Radius + b2.Radius - dist);
+
+            //b1.VX = -b1.VX;
+            //b1.VY = -b1.VY;
+            //b2.VX = -b2.VX;
+            //b2.VY = -b2.VY;
+        }
+
+        protected class Bubble
+        {
+            public string Color { get; set; }
+
+            public double Radius { get; set; }
+
+            public double X { get; set; }
+
+            public double Y { get; set; }
+
+            public double VX { get; set; }
+
+            public double VY { get; set; }
+        }
+    }
+}
---
+++ Services/BoundingBoxSubscriber.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Text.Json.Serialization;
+
+namespace Blazor.Bubbles.Services
+{
+    public class BoundingBoxSubscriberService : IAsyncDisposable
+    {
+        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+
+        public BoundingBoxSubscriberService(IJSRuntime jsRuntime)
+        {
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+               "import", "./js/Blazor.Bubbles.BoundingBoxSubscriber.js").AsTask());
+        }
+
+        public async Task<BoundingBoxSubscriber> CreateSubscriberForChangeAsync()
+        {
+            IJSObjectReference? module = await moduleTask.Value;
+
+            return new BoundingBoxSubscriber(module);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (moduleTask.IsValueCreated)
+            {
+                IJSObjectReference? module = await moduleTask.Value;
+                await module.DisposeAsync();
+            }
+        }
+    }
+
+    public class BoundingBoxSubscriber
+    {
+        private IJSObjectReference Module { get; init; }
+
+        private Action<BoundingBox> Callback { get; set; }
+
+        private DotNetObjectReference<BoundingBoxSubscriber> objRef { get; set; }
+
+        public BoundingBoxSubscriber(IJSObjectReference module)
+        {
+            Module = module;
+            objRef = DotNetObjectReference.Create(this);
+        }
+
+        public async Task Subscribe(ElementReference elementReference, Action<BoundingBox> callback)
+        {
+            Callback += callback;
+
+            await Module.InvokeVoidAsync("SubscribeForChange", elementReference, objRef);
+        }
+
+        [JSInvokable("InvokeCallback")]
+        public void InvokeCallback(BoundingBox boundingBox)
+        {
+            Callback?.Invoke(boundingBox);
+        }
+    }
+
+    public class BoundingBox
+    {
+        [JsonPropertyName("x")]
+        public double X { get; set; }
+
+        [JsonPropertyName("y")]
+        public double Y { get; set; }
+
+        [JsonPropertyName("width")]
+        public double Width { get; set; }
+
+        [JsonPropertyName("Height")]
+        public double Height { get; set; }
+    }
+}
