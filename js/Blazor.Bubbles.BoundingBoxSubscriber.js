3039e93c2288e91c58259d470f39c2c95aef956d
KristofferStrube <KristofferStrube@users.noreply.github.com>
2022-05-10 11:28:40 +0000

    deploy: 493e359f7b16e907d075a963d4892e136e391acd

---
+++ js/Blazor.Bubbles.BoundingBoxSubscriber.js
@@ -0,0 +1,11 @@
+﻿export function SubscribeForChange(element, objRef) {
+    window.addEventListener('resize', () => {
+        objRef.invokeMethod('InvokeCallback', element.getBoundingClientRect());
+    });
+
+    objRef.invokeMethod('InvokeCallback', element.getBoundingClientRect());
+}
+
+export function UnSubscribeForChange() {
+
+}
